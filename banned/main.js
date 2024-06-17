9a63d57697ff571dcf6ef0aaed261e04172dff5d
Yomero Chamba <dokdospanish@gmail.com>
2024-06-17 10:47:54 +0900

    Use banned words from list

--- banned/main.js
+++ banned/main.js
@@ -9,21 +9,32 @@ import fs from "fs";
 import { parseSync } from "subtitle";
 import { Parser } from "my-srt-parser";
 
-const INPUT_DIR_NAME = `${__dirname}/input`;
+const INPUT_DIR_PATH = `${__dirname}/input`;
+const INPUT_FILE_PATH = `${__dirname}/words.txt`;
 
-const allFileNames = fs.readdirSync(INPUT_DIR_NAME);
+const allFileNames = fs.readdirSync(INPUT_DIR_PATH);
 const supportedExtensions = ["srt", "vtt"];
 const validFileNames = allFileNames.filter(
   (s) => !!supportedExtensions.includes(s.split(".").pop())
 );
 
-const parser = new Parser(); // ONLY for ms -> timestamp conversion
+const bannedWords = readWordsFromFile(INPUT_FILE_PATH);
+
+if (!bannedWords?.length) {
+  console.log(`\x1b[31m There are no banned words. \x1b[0m`);
+  process.exit();
+} else {
+  console.log(
+    `\x1b[45m Banned words (${bannedWords.length}): ${bannedWords} \x1b[0m\n`
+  );
+}
+
+const bannedRegex = new RegExp("\\b(?:" + bannedWords.join("|") + ")\\b", "gi");
 
 let found = [];
-const bannedRegex = /Teyong|Taeyoung|Heyoung|Haeyoung/gi;
-
+const parser = new Parser(); // ONLY for ms -> timestamp conversion
 for (let fileName of validFileNames) {
-  const rawContent = fs.readFileSync(`${INPUT_DIR_NAME}/${fileName}`, "utf8");
+  const rawContent = fs.readFileSync(`${INPUT_DIR_PATH}/${fileName}`, "utf8");
   const parsedContent = parseSync(rawContent);
   const cues = parsedContent.filter((line) => line.type === "cue");
 
@@ -37,6 +48,7 @@ for (let fileName of validFileNames) {
       result.badCues.push({
         ts: parser.millisecondsToTimestamp(cue.data.start),
         text: cue.data.text,
+        matches: cue.data.text.match(bannedRegex),
       });
     }
   });
@@ -45,12 +57,25 @@ for (let fileName of validFileNames) {
 }
 
 if (!found.length) {
-  console.log(`\x1b[32m No banned terms found. \x1b[0m`);
+  console.log(`\x1b[42m No banned terms found. \x1b[0m`);
 } else {
   console.log(
     `\x1b[41m Banned words found in these files (${found.length}): \x1b[0m`
   );
   console.log(`\x1b[31m ${JSON.stringify(found, null, 2)} \x1b[0m`);
+}
 
-  console.log(parser.millisecondsToTimestamp(1000));
+function readWordsFromFile(filename) {
+  try {
+    const data = fs.readFileSync(filename, "utf8");
+    const wordsArray = data
+      .split("\n")
+      .map((word) => word.trim())
+      .filter((word) => word !== "");
+
+    return wordsArray;
+  } catch (err) {
+    console.error("Error reading file:", err);
+    throw err; // Re-throw the error to handle it where the function is called
+  }
 }
